7546a5e30e184a104098e905b549ad7a56f8f83e
smcclure17 <55333380+smcclure17@users.noreply.github.com>
2021-04-13 12:58:00 -0400

    Update prefect_flow.py

--- prefect_flow.py
+++ prefect_flow.py
@@ -14,7 +14,7 @@ def extract() -> tuple:
             data: pd.Dataframe of raw data
             log: int of the run-id 
     """
-    # pretend that were getting this data from an API or something cool
+    # pretend that were getting this data from an API or something cool and that it would update every now and then
     with open('data/la_vacc_03.json') as f:
         jsons = json.load(f)
     log = jsons["runid"]
@@ -32,15 +32,15 @@ def start_engine(name: str) -> sq.engine.base.Engine:
     return engine
 
 @task
-def check_if_new(data: int, eng: sq.engine.base.Engine) -> bool:
+def check_if_new(runid: int, eng: sq.engine.base.Engine) -> bool:
     """
         checks to see if the run_id matches any of the previous runs. 
         params: runid/log value of the raw data
         return: True if log is unique, and data should be appended to db
                 False if log is not unique, (data is duplicated)
     """
-    log_vals = list(pd.read_sql("select * from log", eng)['runid'])
-    if data not in log_vals: return True
+    log = list(pd.read_sql("select runid from log", eng)['runid'])
+    if runid not in log: return True
     else: return False
 
 @task
@@ -50,14 +50,14 @@ def transform(data: pd.DataFrame) -> pd.DataFrame:
         params: pd.DataFrame containing raw data
         return: pd.DataFrame of normalized/reshaped data
     """
-    # filter the data we want, match col names to database
+    # filter only the columns that we care about
     data = data[["county", "administered_date", "partially_vaccinated", "fully_vaccinated"]]
-    # rename columns
+    # rename the columns to match our database schema
     data.columns = ['county', 'date', 'initiated', 'completed']
     return data
 
 @task 
-def load(data: pd.DataFrame, log: str, eng: sq.engine.base.Engine) -> None:
+def load(data: pd.DataFrame, runid: str, eng: sq.engine.base.Engine) -> None:
     """
         inserts dataframe values into covid_vaccines table and runid into runid table
         params: 
@@ -66,31 +66,38 @@ def load(data: pd.DataFrame, log: str, eng: sq.engine.base.Engine) -> None:
             eng: engine connected to database
         returns: None
     """
-    # append covid data into corresponding table
+    # insert covid data into corresponding table
     data.to_sql('covid_vaccines', eng, index=False, if_exists='append')
     
-    # append run id to list of updates
-    log_row = pd.DataFrame({"runid":[log,]})
-    log_row.to_sql('log', eng, index=False, if_exists='append')
+    # insert runid to list of updates
+    runid_row = pd.DataFrame({"runid":[runid,]})
+    runid_row.to_sql('log', eng, index=False, if_exists='append')
     return None
 
+
 def main():
-    # initiate flow to run every minute
+
+    # create a schedule with a 1 minute interval
     schedule = IntervalSchedule(
         start_date=dt.utcnow() + td(seconds=1),
         interval=td(minutes=1),
     )
+
+    # initiate flow to run every minute
     with Flow("Basic-ETL", schedule) as flow:
+
+        # define the order of each task
         fetched_data = extract()
         engine = start_engine('data')
         check_duplicate = check_if_new(fetched_data[1], engine)
 
-        # only add data if not duplicated
+        # only manipulate and add data if not duplicated
         with case(check_duplicate, True):
             prepped_data = transform(fetched_data[0])
             load(prepped_data, fetched_data[1], engine)
         
         flow.run()
+
 
 if __name__ == "__main__":
     main()
